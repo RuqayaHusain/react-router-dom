08d8133d900d2cf9d0c4c01417fbf1d87f0c671b
Ruqaya Husain <ruqayanader22@gmail.com>
2025-10-25 13:21:38 +0300

    finished from implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react';
+import { Route, Routes } from 'react-router';
+
+//components
 import PokemonList from './components/PokemonList/PokemonList';
+import NavBar from './components/NavBar/NavBar';
+import PokemonDetails from './components/PokemonDetails/PokemonDetails';
+import PokemonForm from './components/PokemonForm/PokemonForm';
 
 const initialState = [
   { _id: 1, name: 'bulbasaur', weight: 69, height: 7 },
@@ -11,10 +17,26 @@ const initialState = [
 
 const App = () => {
   const [pokemon, setPokemon] = useState(initialState);
+
+  const addPokemon = (newPokemonData) => {
+    newPokemonData._id = pokemon.length + 1;
+    setPokemon([...pokemon, newPokemonData]);
+  };
+
   return (
     <>
       <h1>Pokemon!</h1>
-      <PokemonList pokemon={pokemon} />
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<h2>Home Page</h2>} />
+        <Route path='/pokemon' element={<PokemonList pokemon={pokemon} />} />
+        <Route path='/pokemon/new' element={<PokemonForm addPokemon={addPokemon}/>}/>
+        <Route
+          path="/pokemon/:pokemonId"
+          element={<PokemonDetails pokemon={pokemon} />}
+        />
+        <Route path='*' element={<div>404 Not Found</div>} /> {/* default route */}
+      </Routes>
     </>
   );
 };
